af65cd0f6768d0641fbc4cae450fdc655029990d
bersenev05 <107613209+bersenev05@users.noreply.github.com>
2023-10-18 00:05:15 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -3,48 +3,61 @@ from aiogram import Bot, Dispatcher, types, executor
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
 from classes.user import User, admin, cityes
 from classes.event import Event
-from data.base_file import userbase, eventbase
+from data.base_file import userbase, eventbase, sort_eventbase
 import pathlib
 from pathlib import Path
 dir_path = pathlib.Path.cwd()
 
+#МАШИНА СОСТОЯНИЙ
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+storage=MemoryStorage()
+
+class StepsForm(StatesGroup):
+    get_fio=State()
+    get_phone=State()
+    get_date=State()
 
 
 bot = Bot(token = "6100104762:AAFVZRJCVVhtula5Bo6bGlDlpPtjuav7qlI",
           parse_mode="HTML")
-dp = Dispatcher(bot)
+dp = Dispatcher(bot,storage=MemoryStorage())
 
 statisticbot = Bot(token = "6681358573:AAEDPtrd3jNn82es9LS69eDOicI0ih9FSxk",
                    parse_mode="HTML")
 
 
 
-
-
 eventbase["0"] = Event(name="Встреча клуба Росмолодежь.Бизнес",
-                       url_to_tgchat="vk.com/undaunt3d",
+                       url_to_tgchat="https://t.me/+9PY3bcf3ZH01MDZi",
                        photo_path=Path(dir_path, "files", "photo", "rosmol.jpg"),
                        description="Тут будет описание",
                        date = "16.03.2024",
+                       time = "12:00",
                        creator="@vanya_slash",
+                       location = "Культурная станция Гагарин. Маерчака 17",
                        vzletbusiness=True)
 
 eventbase["1"] = Event(name="Я в деле",
-                       url_to_tgchat="vk.com/undaunt3d",
+                       url_to_tgchat="https://t.me/+9PY3bcf3ZH01MDZi",
                        photo_path=Path(dir_path, "files", "photo", "yavdele.jpg"),
                        description="Предпринимательский кейс-чемпионат. Создай свой бизнес-проект с помощью наставников",
                        date = "15.03.2024",
+                       time = "12:00",
                        creator="@vanya_slash",
+                       location = "Культурная станция Гагарин. Маерчака 17",
                        vzletbusiness=True)
 
 eventbase["2"] = Event(name="Битва Креаторов",
-                       url_to_tgchat="vk.com/undaunt3d",
+                       url_to_tgchat="https://t.me/+9PY3bcf3ZH01MDZi",
                        photo_path=Path(dir_path, "files", "photo", "institut.jpg"),
                        description="приходите пж",
                        date = "11.03.2024",
+                       time = "12:00",
                        creator="@vanya_slash",
+                       location = "Культурная станция Гагарин. Маерчака 17",
                        vzletbusiness=True)
-
 
 
 @dp.message_handler(commands=["start"])
@@ -104,7 +117,7 @@ async def city(message: types.CallbackQuery):
 
     #клавиатура
     ikb = InlineKeyboardMarkup()
-    btn1 = InlineKeyboardButton(text = "Мероприятия", callback_data="events")
+    btn1 = InlineKeyboardButton(text = "Мероприятия", callback_data="eventsnext")
     btn2 = InlineKeyboardButton(text = "Обучение", callback_data = "learning")
     btn3 = InlineKeyboardButton(text = "Стажировки", callback_data = "work")
     btn4 = InlineKeyboardButton(text="Взлёт.Бизнес", callback_data="business")
@@ -123,40 +136,44 @@ async def city(message: types.CallbackQuery):
     png.close()
 
 
-@dp.callback_query_handler(text = "events")
-async def events(message: types.CallbackQuery):
+# @dp.callback_query_handler(text = "events")
+# async def events(message: types.CallbackQuery):
+#
+#     # получаем объект класса юзер. Чисто для укорачивания кода
+#     user = userbase[str(message.from_user.id)]
+#
+#     # добавляем действие
+#     await user.AddAction(f"Выбрал {message.data}")
+#
+#     # обнуляем локейшн для дальнейшего движения по карусели
+#     user.location = None
+#
+#     #Сортируем эвенты по датам
+#     global eventbase
+#     eventbase = await sort_eventbase(eventbase)
+#
+#     # клавиатура
+#     ikb = InlineKeyboardMarkup()
+#     btn1 = InlineKeyboardButton(text="Полетели!", callback_data="eventsnext")
+#     btn2 = InlineKeyboardButton(text="◀️ Назад", callback_data=user.city)
+#     ikb.add(btn1).add(btn2)
+#
+#     png = open(Path(dir_path, "files", "photo", "events.png"), "rb")
+#     photo = types.InputMediaPhoto(png, caption="В этом разделе собраны все ближайшие мероприятия в твоём городе.\n\n "
+#                                                "Ты можешь мгновенно зарегистрироваться и получить ссылку на общий чат.\n\n "
+#                                                "Участвуй в мероприятиях и получай баллы активности. "
+#                                                "Ты можешь менять их на скидки и подарки у наших партнеров")
+#
+#     # Редактируем сообщения
+#     await bot.edit_message_media(chat_id=user.id,
+#                                  message_id=user.last_message,
+#                                  media=photo,
+#                                  reply_markup=ikb)
+#
+#     png.close()
 
-    # получаем объект класса юзер. Чисто для укорачивания кода
-    user = userbase[str(message.from_user.id)]
 
-    # добавляем действие
-    await user.AddAction(f"Выбрал {message.data}")
-
-    # обнуляем локейшн для дальнейшего движения по карусели
-    user.location = None
-
-    # клавиатура
-    ikb = InlineKeyboardMarkup()
-    btn1 = InlineKeyboardButton(text="Полетели!", callback_data="eventsnext")
-    btn2 = InlineKeyboardButton(text="◀️ Назад", callback_data=user.city)
-    ikb.add(btn1).add(btn2)
-
-    png = open(Path(dir_path, "files", "photo", "events.png"), "rb")
-    photo = types.InputMediaPhoto(png, caption="В этом разделе собраны все ближайшие мероприятия в твоём городе.\n\n "
-                                               "Ты можешь мгновенно зарегистрироваться и получить ссылку на общий чат.\n\n "
-                                               "Участвуй в мероприятиях и получай баллы активности. "
-                                               "Ты можешь менять их на скидки и подарки у наших партнеров")
-
-    # Редактируем сообщения
-    await bot.edit_message_media(chat_id=user.id,
-                                 message_id=user.last_message,
-                                 media=photo,
-                                 reply_markup=ikb)
-
-    png.close()
-
-
-
+callback_location = [f"event_registration_{index}" for index in list(eventbase.keys())]
 @dp.callback_query_handler(text=["eventsnext","eventsback"])
 async def events1(message: types.CallbackQuery):
 
@@ -164,11 +181,16 @@ async def events1(message: types.CallbackQuery):
     user = userbase[str(message.from_user.id)]
 
     # добавляем действие
-    await user.AddAction(f"кейс ")
+    await user.AddAction(f"листнул на эвент")
+
+    # Сортируем эвенты по датам
+    global eventbase
+    eventbase = await sort_eventbase(eventbase)
 
     #Движение по карусели
     if user.location == None:
         user.location = "0"
+
     elif message.data == "eventsnext":
         if int(user.location) < len(eventbase)-1:
             user.location = str(int(user.location) + 1)
@@ -180,21 +202,22 @@ async def events1(message: types.CallbackQuery):
         else:
             user.location = str(len(eventbase)-1)
 
+    #получаем из базы событие
     event = eventbase[user.location]
 
     # клавиатура
     ikb = InlineKeyboardMarkup()
     btn1 = InlineKeyboardButton(text="Назад", callback_data="eventsback")
     btn2 = InlineKeyboardButton(text="Далее", callback_data="eventsnext")
-    btn3 = InlineKeyboardButton(text="🕹 Зарегистрироваться", callback_data="registration")
-    btn5 = InlineKeyboardButton(text="🗂 Главное меню", callback_data="mainmenu")
+    btn3 = InlineKeyboardButton(text="🕹 Зарегистрироваться", callback_data=f"event_registration_{user.location}")
+    btn5 = InlineKeyboardButton(text="🗂 Главное меню", callback_data=user.city)
     ikb.row(btn1,btn2).add(btn3).add(btn5)
 
 
     png = open(event.photo_path, "rb")
     photo = types.InputMediaPhoto(png, caption=f"<b>{event.name}</b>\n\n"
                                                f"{event.description}\n\n"
-                                               f"<b>Дата:</b> {event.date}")
+                                               f"<b>Дата:</b> {event.date} {event.time}")
 
     # Редактируем сообщения
     await bot.edit_message_media(chat_id=user.id,
@@ -205,9 +228,98 @@ async def events1(message: types.CallbackQuery):
     png.close()
 
 
+@dp.callback_query_handler(text = callback_location)
+async def registration1(message: types.CallbackQuery):
+
+    # получаем объект класса юзер. Чисто для укорачивания кода
+    user = userbase[str(message.from_user.id)]
+
+    user.registration_hub = eventbase[message.data[-1]]
+
+    #добавляем картинку и текст
+    png = open(user.registration_hub.photo_path,"rb")
+    photo = types.InputMediaPhoto(png, caption=f"<i>🕹 Регистрация</i>\n\n"
+                                               f"<b>🎟 Событие:</b>\n<code>{user.registration_hub.name}</code>\n\n"
+                                               f"<b>🗓 Дата:</b>\n<code>{user.registration_hub.date} {user.registration_hub.time}</code>\n\n"
+                                               f"<b>📍 Место:</b>\n<code>{user.registration_hub.location}</code>\n\n"
+                                               f"<b>Введи ФИО</b>")
+
+    # Редактируем сообщения
+    await bot.edit_message_media(chat_id=user.id,
+                                 message_id=user.last_message,
+                                 media=photo)
+
+    await StepsForm.get_fio.set()
 
 
+@dp.message_handler(state=StepsForm.get_fio)
+async def registration2(message: types.Message, state: FSMContext):
 
+    # получаем объект класса юзер. Чисто для укорачивания кода
+    user = userbase[str(message.from_user.id)]
+
+    #удаляем сообщение пользователя
+    await message.delete()
+
+    #Добавляем ФИО в базу
+    user.fio = message.text
+
+    # добавляем картинку и текст
+    png = open(user.registration_hub.photo_path, "rb")
+    photo = types.InputMediaPhoto(png, caption=f"<i>🕹 Регистрация</i>\n\n"
+                                               f"<b>🎟 Событие:</b>\n<code>{user.registration_hub.name}</code>\n\n"
+                                               f"<b>🗓 Дата:</b>\n<code>{user.registration_hub.date} {user.registration_hub.time}</code>\n\n"
+                                               f"<b>📍 Место:</b>\n<code>{user.registration_hub.location}</code>\n\n"
+                                               f"<b>🙎‍♂️ ФИО:</b>\n<code>{user.fio}</code>\n\n"
+                                               f"<b>Введи номер телефона</b>")
+
+    # Редактируем сообщения
+    await bot.edit_message_media(chat_id=user.id,
+                                 message_id=user.last_message,
+                                 media=photo)
+
+    await StepsForm.get_phone.set()
+
+
+@dp.message_handler(state=StepsForm.get_phone)
+async def registration3(message: types.Message, state: FSMContext):
+
+    # получаем объект класса юзер. Чисто для укорачивания кода
+    user = userbase[str(message.from_user.id)]
+
+    #удаляем сообщение пользователя
+    await message.delete()
+
+    #Добавляем ФИО в базу
+    user.phone = message.text
+
+    ikb = InlineKeyboardMarkup()
+    btn1 = InlineKeyboardButton(text="🔗 Общий чат мероприятия", url=user.registration_hub.url_to_tgchat)
+    btn2 = InlineKeyboardButton(text="🗂 Главное меню", callback_data=user.city)
+    ikb.row(btn1).add(btn2)
+
+    # добавляем картинку и текст
+    png = open(user.registration_hub.photo_path, "rb")
+    photo = types.InputMediaPhoto(png, caption=f"<i>🕹 Регистрация</i>\n\n"
+                                               f"<b>🎟 Событие:</b>\n<code>{user.registration_hub.name}</code>\n\n"
+                                               f"<b>🗓 Дата:</b>\n<code>{user.registration_hub.date} {user.registration_hub.time}</code>\n\n"
+                                               f"<b>📍 Место:</b>\n<code>{user.registration_hub.location}</code>\n\n"
+                                               f"<b>🙎‍♂️ ФИО:</b>\n<code>{user.fio}</code>\n\n"
+                                               f"<b>️📱 Номер:</b>\n<code>{user.phone}</code>\n\n"
+                                               f"<b>️--------------------------</b>\n"
+                                               f"<b>🎉 Ты успешно зарегистрирован!</b> <i>Перешли это сообщение в избранное, "
+                                               f"чтобы ничего не забыть</i>\n\n")
+
+    user.registration_hub.registrations.append(user)
+    user.registration_hub = None
+
+    # Редактируем сообщения
+    await bot.edit_message_media(chat_id=user.id,
+                                 message_id=user.last_message,
+                                 media=photo,
+                                 reply_markup=ikb)
+
+    await state.finish()
 
 
 
