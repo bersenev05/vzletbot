f1ff31e53ef8773b55385742083bc57a893e9d22
bersenev05 <107613209+bersenev05@users.noreply.github.com>
2023-11-02 22:13:07 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ class AddEvent(StatesGroup):
     get_photo = State()
 
 
-bot = Bot(token = "6100104762:AAGgp62VNxUM7b99dNUua_7oza3AWceajYE",
+bot = Bot(token = "6655005383:AAHs-6kFCkl12zAIp1TVB4m4sXud1GBZUO4",
           parse_mode="HTML")
 dp = Dispatcher(bot,storage=MemoryStorage())
 
@@ -40,39 +40,49 @@ statisticbot = Bot(token = "6681358573:AAEDPtrd3jNn82es9LS69eDOicI0ih9FSxk",
                    parse_mode="HTML")
 
 
-workbase["0"] = Work(name = "c# Разработчик в Инжинеры Продаж",
+workbase["0"] = Work(name = "Менеджер по работе с партнерами Вконтакте",
                      url_to_tgchat = "https://t.me/+9PY3bcf3ZH01MDZi",
-                     photo_path = Path(dir_path, "files", "photo", "rosmol.jpg"),
-                     description = "Требуется",
-                     date = "16.03.2025",
+                     photo_path = Path(dir_path, "files", "photo","events", "vk-min.png"),
+                     description = "Стажёр будет участвовать в разработке концепций и интеграций сервисов и продуктов ВКонтакте с партнёрами в направлении «Образование и книги»",
+                     date = "19.11.2023",
                      time = "12:00",
                      creator = "5965231899",
                      location = "online",
                      vzletbusiness = True)
 
-workbase["1"] = Work(name = "Разработка программного обеспечения на Python. Цифровая кафедра СФУ",
+workbase["1"] = Work(name = "Flutter-разработчик в Яндекс",
                      url_to_tgchat = "https://t.me/+9PY3bcf3ZH01MDZi",
                      photo_path = Path(dir_path, "files", "photo", "python.jpg"),
-                     description = "Бесплатное обучение! Описание...",
-                     date = "16.03.2025",
+                     description = "Открыт набор на осеннюю оплачиваемую стажировку. Подавайте заявки! Если вы отлично себя проявите, можете продолжить работать с нами. Более 50% стажёров становятся штатными сотрудниками Яндекса.",
+                     date = "20.12.2023",
                      time = "12:00",
                      creator = "5965231899",
                      location = "online",
                      vzletbusiness = True)
 
-learningbase["0"] = Learning(name = "Разработка мобильных приложений на Flutter. Цифровая кафедра СФУ",
+learningbase["0"] = Learning(name = "Школа анализа данных от Яндекса",
                            url_to_tgchat = "https://t.me/+9PY3bcf3ZH01MDZi",
-                           photo_path = Path(dir_path, "files", "photo", "flutter.jpg"),
-                           description = "Бесплатное обучение! Описание...",
-                           date = "16.03.2025",
+                           photo_path = Path(dir_path, "files", "photo", "events" , "shad-min.png"),
+                           description = "Двухгодичная программа, на которой вы научитесь разрабатывать сервисы на базе ML, анализировать данные, создавать системы хранения и обработки больших данных и многое другое",
+                           date = "12.11.2023",
                            time = "12:00",
                            creator = "5965231899",
                            location = "online",
                            vzletbusiness = True)
 
-learningbase["1"] = Learning(name = "Разработка программного обеспечения на Python. Цифровая кафедра СФУ",
+learningbase["1"] = Learning(name = "Тинькофф.Финтех - онлайн курс по бизнес-анализу",
                            url_to_tgchat = "https://t.me/+9PY3bcf3ZH01MDZi",
-                           photo_path = Path(dir_path, "files", "photo", "python.jpg"),
+                           photo_path = Path(dir_path, "files", "photo", "events","tinkoff-min.jpg"),
+                           description = "Проводим обучение и оплачиваемые стажировки по направлениям аналитики, бэкенд- и фронтенд-разработки, QA, маркетинга и другим",
+                           date = "16.12.2023",
+                           time = "12:00",
+                           creator = "5965231899",
+                           location = "online",
+                           vzletbusiness = True)
+
+learningbase["2"] = Learning(name = "Разработка программного обеспечения на Python. Цифровая кафедра СФУ",
+                           url_to_tgchat = "https://t.me/+9PY3bcf3ZH01MDZi",
+                           photo_path = Path(dir_path, "files", "photo", "events" , "python-min.jpg"),
                            description = "Бесплатное обучение! Описание...",
                            date = "16.03.2025",
                            time = "12:00",
@@ -82,9 +92,9 @@ learningbase["1"] = Learning(name = "Разработка программног
 
 eventbase["0"] = Event(name="Встреча клуба Росмолодежь.Бизнес",
                        url_to_tgchat="https://t.me/+9PY3bcf3ZH01MDZi",
-                       photo_path=Path(dir_path, "files", "photo", "rosmol.jpg"),
-                       description="Тут будет описание",
-                       date = "16.03.2024",
+                       photo_path=Path(dir_path, "files", "photo", "events", "rosmolbusiness-min.jpg"),
+                       description="Мощнейшее объединение молодых предпринимателей твоего города. Проводим встречи, мастермайнды, встречи с большим бизнесом и т.д.",
+                       date = "4.11.2023",
                        time = "12:00",
                        creator="5965231899",
                        location = "Культурная станция Гагарин. Маерчака 17",
@@ -92,22 +102,32 @@ eventbase["0"] = Event(name="Встреча клуба Росмолодежь.Б
 
 eventbase["1"] = Event(name="Я в деле",
                        url_to_tgchat="https://t.me/+9PY3bcf3ZH01MDZi",
-                       photo_path=Path(dir_path, "files", "photo", "yavdele.jpg"),
-                       description="Предпринимательский кейс-чемпионат. Создай свой бизнес-проект с помощью наставников",
-                       date = "15.03.2024",
+                       photo_path=Path(dir_path, "files", "photo", "events" , "yavdele-min.jpg"),
+                       description="Предпринимательский кейс-чемпионат. Создай свой бизнес-проект с помощью наставников. Победитель регионального этапа едет в Москву!",
+                       date = "5.11.2023",
                        time = "12:00",
                        creator="5965231899",
-                       location = "Культурная станция Гагарин. Маерчака 17",
+                       location = "Точка Кипения. Квант",
                        vzletbusiness=True)
 
 eventbase["2"] = Event(name="Битва Креаторов",
                        url_to_tgchat="https://t.me/+9PY3bcf3ZH01MDZi",
-                       photo_path=Path(dir_path, "files", "photo", "institut.jpg"),
-                       description="приходите пж",
-                       date = "11.03.2024",
+                       photo_path=Path(dir_path, "files", "photo", "events", "bitva-min.jpg"),
+                       description="Первое в Красноярске реалити-шоу о креативных предпринимателях Сибири. Проходи отбор! Пять победителей получат по миллиону рублей!",
+                       date = "6.11.2023",
                        time = "12:00",
                        creator="5965231899",
-                       location = "Культурная станция Гагарин. Маерчака 17",
+                       location = "Креативный кластер Квадрат",
+                       vzletbusiness=True)
+
+eventbase["3"] = Event(name = "Красноярский экономический форум",
+                       url_to_tgchat="https://t.me/+9PY3bcf3ZH01MDZi",
+                       photo_path=Path(dir_path, "files", "photo", "events", "kef-min.jpg"),
+                       description="Самый масштабный в Сибири форум об экономическом развитии страны. Будет много спикеров и интересных площадок. Обсудим взаимодействие госудаства с бизнесом.",
+                       date="7.11.2023",
+                       time="12:00",
+                       creator="5965231899",
+                       location="Конгресс-холл СФУ",
                        vzletbusiness=True)
 
 #Старт
